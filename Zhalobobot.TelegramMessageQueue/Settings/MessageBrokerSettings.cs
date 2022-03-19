94b03b052ffe95ea1ea51ccf75774a8acb56279f
d.stukov <d.stukov@skbkontur.ru>
2022-03-19 14:00:59 +0500

    lowered limits

--- Zhalobobot.TelegramMessageQueue/Settings/MessageBrokerSettings.cs
+++ Zhalobobot.TelegramMessageQueue/Settings/MessageBrokerSettings.cs
@@ -2,6 +2,6 @@ namespace Zhalobobot.TelegramMessageQueue.Settings;
 
 public static class MessageBrokerSettings
 {
-    internal static int UserMessagesLimit { get; set; } = 25; // note (d.stukov): max allowed ~30
-    internal static int GroupMessagesPerMinuteLimit { get; set; } = 16; // note (d.stukov): max allowed ~20
+    internal static int UserMessagesLimit { get; set; } = 20; // note (d.stukov): max allowed ~30
+    internal static int GroupMessagesPerMinuteLimit { get; set; } = 14; // note (d.stukov): max allowed ~20
 }
